992ce8acbbf56beb53b1b755b345c11e6e04f550
swarajpumex <swaraj@pumexinfotech.com>
2026-05-26 23:17:36 +0530

    Current cpanel code

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-    $config['base_url'] = 'https://cinemanewsagency.com/';
-//$config['base_url'] = 'http://localhost/cinemanews/';
+// $config['base_url'] = 'https://cinemanewsagency.com/';
+$config['base_url'] = 'http://localhost/';
 
 /*
 |--------------------------------------------------------------------------
@@ -39,8 +39,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['index_page'] = 'index.php';
 
 
-$config['admin_title'] 			="Cinema News Agency Admin"; // this will come all the admin page title text.
-$config['admin_controller'] 	="CinemaAd"; // this is the default admin controller.
+$config['admin_title']             = "Cinema News Agency Admin"; // this will come all the admin page title text.
+$config['admin_controller']     = "CinemaAd"; // this is the default admin controller.
 
 /*
 |--------------------------------------------------------------------------
@@ -57,7 +57,7 @@ $config['admin_controller'] 	="CinemaAd"; // this is the default admin controlle
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']    = 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
@@ -81,7 +81,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']    = 'english';
 
 /*
 |--------------------------------------------------------------------------
@@ -228,7 +228,7 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 4;
 
 /*
 |--------------------------------------------------------------------------
@@ -384,12 +384,24 @@ $config['encryption_key'] = 'CinemaNewsAgency2026SecureKey98765';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
+$config['sess_save_path'] = APPPATH . 'cache/sessions';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
+
+
+
+
+$config['sess_use_database'] = ($config['sess_driver'] === 'database');
+$config['sess_table_name'] = ($config['sess_driver'] === 'database' && ! empty($config['sess_save_path']))
+    ? $config['sess_save_path']
+    : 'ci_sessions';
+$config['sess_encrypt_cookie'] = FALSE;
+$config['sess_expire_on_close'] = FALSE;
+$config['sess_match_useragent'] = TRUE;
+$config['time_reference'] = 'local';
 /*
 |--------------------------------------------------------------------------
 | Cookie Related Variables
@@ -405,11 +417,11 @@ $config['sess_regenerate_destroy'] = FALSE;
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
+$config['cookie_prefix']    = '';
+$config['cookie_domain']    = '';
+$config['cookie_path']        = '/';
+$config['cookie_secure']    = FALSE;
+$config['cookie_httponly']     = FALSE;
 
 /*
 |--------------------------------------------------------------------------
--- application/views/admin_panel/navigation-menu.php
+++ application/views/admin_panel/navigation-menu.php
@@ -1,246 +1,257 @@
-
 <!----- If usergroup table authority field is ADMIN this should come first at menu navigation--->
-<?php if($_SESSION['ADMIN_USER_TYPE'] !="SUPER_ADMIN"): ?>	 
-	 <?php 
+<?php
+$CI = &get_instance();
+$adminUserType = '';
 
-	 		$mainMenuId =0;
-			$isSub =FALSE;
-			$isSubAdded =FALSE;
-		                        
-			foreach($admin_navigation  as $row):
-				
-				if($mainMenuId!=$row["Id"]):
-				 $mainMenuId=$row["Id"]	;
-                                 
-                                 if($row["SubMenuItem"]=="NULL"):
-                                     $db = new Database();
-                                     $where = "MenuItemId='{$mainMenuId}' AND UserGroup='" . $_SESSION['ADMIN_USER_TYPE'] . "'";
-                                     $rStatus  = $db->getFieldValueById("sys_user_group_menu", "Status", $where); // getting menu right status
+if (isset($CI->session)) {
+	$adminUserType = $CI->session->userdata('ADMIN_USER_TYPE');
+}
+
+if (($adminUserType === FALSE || $adminUserType === '' || is_null($adminUserType)) && isset($_SESSION['ADMIN_USER_TYPE'])) {
+	$adminUserType = $_SESSION['ADMIN_USER_TYPE'];
+}
+?>
+
+<?php if ($adminUserType != "SUPER_ADMIN"): ?>
+	<?php
+
+	$mainMenuId = 0;
+	$isSub = FALSE;
+	$isSubAdded = FALSE;
+
+	foreach ($admin_navigation  as $row):
+
+		if ($mainMenuId != $row["Id"]):
+			$mainMenuId = $row["Id"];
+
+			if ($row["SubMenuItem"] == "NULL"):
+				$db = new Database();
+				$where = "MenuItemId='{$mainMenuId}' AND UserGroup='" . $adminUserType . "'";
+				$rStatus  = $db->getFieldValueById("sys_user_group_menu", "Status", $where); // getting menu right status
 	?>
-			<?php if($row["MainStatus"]=="Showing"):?>  <!-- Main Status start -->
-                                <?php if($rStatus=="1"):?>  <!-- Right status start  -->
-				<li>
-          		<a href="<?php echo base_url() .$row["linkPage"];?>">
-				<i class="<?php echo $row["MainIcon"];?>"></i> <span><?php echo $row["MainMenu"];?></span>
-				<?php if(!empty($row["countTitle"])):
-                                   $count =0; 
-                                   if(!empty($row["countTable"]))
-                                   {       
+				<?php if ($row["MainStatus"] == "Showing"): ?> <!-- Main Status start -->
+					<?php if ($rStatus == "1"): ?> <!-- Right status start  -->
+						<li>
+							<a href="<?php echo base_url() . $row["linkPage"]; ?>">
+								<i class="<?php echo $row["MainIcon"]; ?>"></i> <span><?php echo $row["MainMenu"]; ?></span>
+								<?php if (!empty($row["countTitle"])):
+									$count = 0;
+									if (!empty($row["countTable"])) {
 										$db = new Database();
-										$countWhere =$row["countWhereClause"];
+										$countWhere = $row["countWhereClause"];
 										$count = $db->totalCount("Id", $row["countTable"], $countWhere);
 										$db = NULL;
-                                   }         
-                                ?>
-				<small class="label pull-right bg-green" title="<?php echo $row["countTitle"];?>"><?php echo $count; ?></small>
-				<?php endif;?>
-				
-          		</a>
-        		</li>
-			 <?php endif;?> <!-- Main Status ends -->
-                         <?php endif;?> <!-- Right Status ends -->
-                         
-    		 <?php endif; // if SubMenuItem ending ?>
-	
-	<?php
-			if($row["SubMenuItem"]!="NULL"): // if submenu item not null
-                                $db = new Database();
-                                $where = "MenuItemId='{$mainMenuId}' AND UserGroup='" . $_SESSION['ADMIN_USER_TYPE'] . "'";
-                                $rStatus  = $db->getFieldValueById("sys_user_group_menu", "Status", $where); // getting menu right status
-                                
-				$CI =& get_instance();
-				$CI->load->model('AdminModel');
-				$result = $CI->AdminModel->getSubMenuItem($row["Id"]);
-				
-				$total = $result->num_rows();
-				$subMenu = $result->result_array();
-				
-    ?>
-		<?php if($row["MainStatus"]=="Showing"):?>  <!-- Main Status start -->
-                <?php if($rStatus=="1"):?>  <!-- Right status start  -->
-			<li class="treeview">
-          		<a href="#">
-            		<i class="<?php echo $row["MainIcon"];?>">
-					</i> <span><?php echo $row["MainMenu"];?></span> 
-					<?php if(!empty($row["countTitle"])):
-                                         $count =0; 
-                                            if(!empty($row["countTable"]))
-                                            {       
-                                            $db = new Database();
-                                            $countWhere =$row["countWhereClause"];
-					    $count = $db->totalCount("Id", $row["countTable"], $countWhere);
-                                            $db = NULL;
-                                            }  
-                                            
-                                        ?>
-					<small class="label pull-right bg-green" title="<?php echo $row["countTitle"];?>"><?php echo $count;?></small>
-					<?php endif;?>
-					<i class="fa fa-angle-left pull-right"></i>
-          		</a>
-				<ul class="treeview-menu">
-				<?php 
-					$countMenu=0;
-					foreach($subMenu  as $subRow): // sub menu loop starts
-					 $countMenu++;
-					  if($subRow["SubStatus"]==1): // sub status start 
-                                            
-			     ?>		  
-						<li class="active">
-							<a href="<?php echo base_url(). $adminController . "/" . $subRow["SubPageLink"]; ?>">
-								<i class="<?php echo $subRow["SubIcon"];?>"></i>
-								<?php echo $subRow["SubMenuItem"];?>
-				
-							</a>
-						</li>
-					<?php if($countMenu==$total):?>
-							</ul></li>
-					<?php endif;?>	
-				<?php endif;?>	<!-- sub status ends -->
-					
-				<?php endforeach;?><!-- sub menu loop ends -->
-			<?php endif;?><!-- Right status ends -->		
-			<?php endif;?><!-- main status ends -->	
-			
-			 		
-	<?php	
-			endif; // if SubMenuItem not null ending
-			endif;
-			endforeach;
-			
-	?>
-                       
-	
-	<!----- If login table usertype field is SUPER_ADMIN : this should come last at menu navigation-->
-	<?php elseif($_SESSION['ADMIN_USER_TYPE'] =="SUPER_ADMIN"): ?>
-	
-        <!----- all menus ------------------------------------------------------->
-        <?php 
+									}
+								?>
+									<small class="label pull-right bg-green" title="<?php echo $row["countTitle"]; ?>"><?php echo $count; ?></small>
+								<?php endif; ?>
 
-	 		$mainMenuId =0;
-			$isSub =FALSE;
-			$isSubAdded =FALSE;
-                        
-			foreach($admin_navigation  as $row):
-				
-				if($mainMenuId!=$row["Id"]):
-				 $mainMenuId=$row["Id"]	;
-                                 
-                                 if($row["SubMenuItem"]=="NULL"):
-                                     
-	?>
-			<?php if($row["MainStatus"]=="Showing"):?>  <!-- Main Status start -->
-                                
-				<li>
-          		<a href="<?php echo base_url() . $row["linkPage"];?>">
-				<i class="<?php echo $row["MainIcon"];?>"></i> <span><?php echo $row["MainMenu"];?></span>
-				<?php if(!empty($row["countTitle"])):
-                                   $count =0; 
-                                   if(!empty($row["countTable"]))
-                                   {       
-                                    $db = new Database();
-                                    $countWhere =$row["countWhereClause"];
-				    $count = $db->totalCount("Id", $row["countTable"], $countWhere);
-                                    $db = NULL;
-                                   }         
-                                ?>
-				<small class="label pull-right bg-green" title="<?php echo $row["countTitle"];?>"><?php echo $count; ?></small>
-				<?php endif;?>
-				
-          		</a>
-        		</li>
-			 <?php endif;?> <!-- Main Status ends -->
-                         
-                         
-    		 <?php endif; // if SubMenuItem ending ?>
-	
-	<?php
-			if($row["SubMenuItem"]!="NULL"): // if submenu item not null
-                                
-				$CI =& get_instance();
-				$CI->load->model('AdminModel');
-				$result = $CI->AdminModel->getSubMenuItem($row["Id"]);
-				
-				$total = $result->num_rows();
-				$subMenu = $result->result_array();
-				
-    ?>
-		<?php if($row["MainStatus"]=="Showing"):?>  <!-- Main Status start -->
-                
-			<li class="treeview">
-          		<a href="#">
-            		<i class="<?php echo $row["MainIcon"];?>">
-					</i> <span><?php echo $row["MainMenu"];?></span> 
-					<?php if(!empty($row["countTitle"])):
-                                         $count =0; 
-                                            if(!empty($row["countTable"]))
-                                            {       
-                                            $db = new Database();
-                                            $countWhere =$row["countWhereClause"];
-					    $count = $db->totalCount("Id", $row["countTable"], $countWhere);
-                                            $db = NULL;
-                                            }  
-                                            
-                                        ?>
-					<small class="label pull-right bg-green" title="<?php echo $row["countTitle"];?>"><?php echo $count;?></small>
-					<?php endif;?>
-					<i class="fa fa-angle-left pull-right"></i>
-          		</a>
-				<ul class="treeview-menu">
-				<?php 
-					$countMenu=0;
-					foreach($subMenu  as $subRow): // sub menu loop starts
-					 $countMenu++;
-					  if($subRow["SubStatus"]==1): // sub status start 
-			     ?>		  
-						<li class="active">
-							<a href="<?php echo base_url(). $adminController . "/" . $subRow["SubPageLink"]; ?>">
-								<i class="<?php echo $subRow["SubIcon"];?>"></i>
-								<?php echo $subRow["SubMenuItem"];?>
 							</a>
 						</li>
-					<?php if($countMenu==$total):?>
-							</ul></li>
-					<?php endif;?>	
-				<?php endif;?>	<!-- sub status ends -->
-					
-				<?php endforeach;?><!-- sub menu loop ends -->
-					
-			<?php endif;?><!-- main status ends -->	
-			
-			 		
-	<?php	
+					<?php endif; ?> <!-- Main Status ends -->
+				<?php endif; ?> <!-- Right Status ends -->
+
+			<?php endif; // if SubMenuItem ending 
+			?>
+
+			<?php
+			if ($row["SubMenuItem"] != "NULL"): // if submenu item not null
+				$db = new Database();
+				$where = "MenuItemId='{$mainMenuId}' AND UserGroup='" . $adminUserType . "'";
+				$rStatus  = $db->getFieldValueById("sys_user_group_menu", "Status", $where); // getting menu right status
+
+				$CI = &get_instance();
+				$CI->load->model('AdminModel');
+				$result = $CI->AdminModel->getSubMenuItem($row["Id"]);
+
+				$total = $result->num_rows();
+				$subMenu = $result->result_array();
+
+			?>
+				<?php if ($row["MainStatus"] == "Showing"): ?> <!-- Main Status start -->
+					<?php if ($rStatus == "1"): ?> <!-- Right status start  -->
+						<li class="treeview">
+							<a href="#">
+								<i class="<?php echo $row["MainIcon"]; ?>">
+								</i> <span><?php echo $row["MainMenu"]; ?></span>
+								<?php if (!empty($row["countTitle"])):
+									$count = 0;
+									if (!empty($row["countTable"])) {
+										$db = new Database();
+										$countWhere = $row["countWhereClause"];
+										$count = $db->totalCount("Id", $row["countTable"], $countWhere);
+										$db = NULL;
+									}
+
+								?>
+									<small class="label pull-right bg-green" title="<?php echo $row["countTitle"]; ?>"><?php echo $count; ?></small>
+								<?php endif; ?>
+								<i class="fa fa-angle-left pull-right"></i>
+							</a>
+							<ul class="treeview-menu">
+								<?php
+								$countMenu = 0;
+								foreach ($subMenu  as $subRow): // sub menu loop starts
+									$countMenu++;
+									if ($subRow["SubStatus"] == 1): // sub status start 
+
+								?>
+										<li class="active">
+											<a href="<?php echo base_url() . $adminController . "/" . $subRow["SubPageLink"]; ?>">
+												<i class="<?php echo $subRow["SubIcon"]; ?>"></i>
+												<?php echo $subRow["SubMenuItem"]; ?>
+
+											</a>
+										</li>
+										<?php if ($countMenu == $total): ?>
+							</ul>
+						</li>
+					<?php endif; ?>
+				<?php endif; ?> <!-- sub status ends -->
+
+			<?php endforeach; ?><!-- sub menu loop ends -->
+		<?php endif; ?><!-- Right status ends -->
+	<?php endif; ?><!-- main status ends -->
+
+
+<?php
 			endif; // if SubMenuItem not null ending
-			endif;
-			endforeach;
-			
+		endif;
+	endforeach;
+
+?>
+
+
+<!----- If login table usertype field is SUPER_ADMIN : this should come last at menu navigation-->
+<?php elseif ($adminUserType == "SUPER_ADMIN"): ?>
+
+	<!----- all menus ------------------------------------------------------->
+	<?php
+
+	$mainMenuId = 0;
+	$isSub = FALSE;
+	$isSubAdded = FALSE;
+
+	foreach ($admin_navigation  as $row):
+
+		if ($mainMenuId != $row["Id"]):
+			$mainMenuId = $row["Id"];
+
+			if ($row["SubMenuItem"] == "NULL"):
+
 	?>
-        
-        
-        <!----- all menus ending --------------------------------------------------->
-        
-        
-        
-        
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-bug"></i>
-            <span>Super Admin</span>
-            <i class="fa fa-angle-left pull-right"></i>
-          </a>
-         <ul class="treeview-menu">
-            <li><a href="<?php echo base_url(). $adminController; ?>/adminMainMenu" title="Add or edit new admin main navigation menu item"><i class="fa fa-circle-o"></i>Admin Main menu item</a></li>
-            <li><a href="<?php echo base_url(). $adminController; ?>/adminSubMenu" title="Add or edit new admin main navigation sub menu item"><i class="fa fa-circle-o"></i>Admin Sub menu item</a></li>
-	    <li><a href="#" title="Add or edit new site main navigation menu item"><i class="fa fa-circle-o"></i>Site Main menu item</a></li>
-            <li><a href="#" title="Add or edit new site main navigation sub menu item"><i class="fa fa-circle-o"></i>Site Sub menu item</a></li>
-	    <li><a href="#" title="Change the log of the site"><i class="fa fa-circle-o"></i>Upload site menu logo</a></li>
-	    <li><a href="#" title="Add or edit user group and rights"><i class="fa fa-circle-o"></i>User group</a></li>
-	    <li><a href="<?php echo base_url(). $adminController; ?>/userLog" title="Shows the site user log"><i class="fa fa-circle-o"></i>User log</a></li>
-	     <li><a href="#" title="Shows the site user log details"><i class="fa fa-circle-o"></i>User log details</a></li>
-	    <li><a href="#" title="Shows the site error log and download the errorlog.txt file"><i class="fa fa-circle-o"></i>Error log</a></li>
-	     <li><a href="#" title="Change the images in pages"><i class="fa fa-circle-o"></i>Site image upload</a></li>
-	     <li><a href="#" title="Add or edit site gallery images"><i class="fa fa-circle-o"></i>Gallery</a></li>
-	    <li><a href="#" title="Shows the complaints and suggessions"><i class="fa fa-circle-o"></i>Complaints & Suggessions</a></li>	
-          </ul>
-        </li>
-        <?php endif; ?>
-	
+				<?php if ($row["MainStatus"] == "Showing"): ?> <!-- Main Status start -->
+
+					<li>
+						<a href="<?php echo base_url() . $row["linkPage"]; ?>">
+							<i class="<?php echo $row["MainIcon"]; ?>"></i> <span><?php echo $row["MainMenu"]; ?></span>
+							<?php if (!empty($row["countTitle"])):
+								$count = 0;
+								if (!empty($row["countTable"])) {
+									$db = new Database();
+									$countWhere = $row["countWhereClause"];
+									$count = $db->totalCount("Id", $row["countTable"], $countWhere);
+									$db = NULL;
+								}
+							?>
+								<small class="label pull-right bg-green" title="<?php echo $row["countTitle"]; ?>"><?php echo $count; ?></small>
+							<?php endif; ?>
+
+						</a>
+					</li>
+				<?php endif; ?> <!-- Main Status ends -->
+
+
+			<?php endif; // if SubMenuItem ending 
+			?>
+
+			<?php
+			if ($row["SubMenuItem"] != "NULL"): // if submenu item not null
+
+				$CI = &get_instance();
+				$CI->load->model('AdminModel');
+				$result = $CI->AdminModel->getSubMenuItem($row["Id"]);
+
+				$total = $result->num_rows();
+				$subMenu = $result->result_array();
+
+			?>
+				<?php if ($row["MainStatus"] == "Showing"): ?> <!-- Main Status start -->
+
+					<li class="treeview">
+						<a href="#">
+							<i class="<?php echo $row["MainIcon"]; ?>">
+							</i> <span><?php echo $row["MainMenu"]; ?></span>
+							<?php if (!empty($row["countTitle"])):
+								$count = 0;
+								if (!empty($row["countTable"])) {
+									$db = new Database();
+									$countWhere = $row["countWhereClause"];
+									$count = $db->totalCount("Id", $row["countTable"], $countWhere);
+									$db = NULL;
+								}
+
+							?>
+								<small class="label pull-right bg-green" title="<?php echo $row["countTitle"]; ?>"><?php echo $count; ?></small>
+							<?php endif; ?>
+							<i class="fa fa-angle-left pull-right"></i>
+						</a>
+						<ul class="treeview-menu">
+							<?php
+							$countMenu = 0;
+							foreach ($subMenu  as $subRow): // sub menu loop starts
+								$countMenu++;
+								if ($subRow["SubStatus"] == 1): // sub status start 
+							?>
+									<li class="active">
+										<a href="<?php echo base_url() . $adminController . "/" . $subRow["SubPageLink"]; ?>">
+											<i class="<?php echo $subRow["SubIcon"]; ?>"></i>
+											<?php echo $subRow["SubMenuItem"]; ?>
+										</a>
+									</li>
+									<?php if ($countMenu == $total): ?>
+						</ul>
+					</li>
+				<?php endif; ?>
+			<?php endif; ?> <!-- sub status ends -->
+
+		<?php endforeach; ?><!-- sub menu loop ends -->
+
+	<?php endif; ?><!-- main status ends -->
+
+
+<?php
+			endif; // if SubMenuItem not null ending
+		endif;
+	endforeach;
+
+?>
+
+
+<!----- all menus ending --------------------------------------------------->
+
+
+
+
+<li class="treeview">
+	<a href="#">
+		<i class="fa fa-bug"></i>
+		<span>Super Admin</span>
+		<i class="fa fa-angle-left pull-right"></i>
+	</a>
+	<ul class="treeview-menu">
+		<li><a href="<?php echo base_url() . $adminController; ?>/adminMainMenu" title="Add or edit new admin main navigation menu item"><i class="fa fa-circle-o"></i>Admin Main menu item</a></li>
+		<li><a href="<?php echo base_url() . $adminController; ?>/adminSubMenu" title="Add or edit new admin main navigation sub menu item"><i class="fa fa-circle-o"></i>Admin Sub menu item</a></li>
+		<li><a href="#" title="Add or edit new site main navigation menu item"><i class="fa fa-circle-o"></i>Site Main menu item</a></li>
+		<li><a href="#" title="Add or edit new site main navigation sub menu item"><i class="fa fa-circle-o"></i>Site Sub menu item</a></li>
+		<li><a href="#" title="Change the log of the site"><i class="fa fa-circle-o"></i>Upload site menu logo</a></li>
+		<li><a href="#" title="Add or edit user group and rights"><i class="fa fa-circle-o"></i>User group</a></li>
+		<li><a href="<?php echo base_url() . $adminController; ?>/userLog" title="Shows the site user log"><i class="fa fa-circle-o"></i>User log</a></li>
+		<li><a href="#" title="Shows the site user log details"><i class="fa fa-circle-o"></i>User log details</a></li>
+		<li><a href="#" title="Shows the site error log and download the errorlog.txt file"><i class="fa fa-circle-o"></i>Error log</a></li>
+		<li><a href="#" title="Change the images in pages"><i class="fa fa-circle-o"></i>Site image upload</a></li>
+		<li><a href="#" title="Add or edit site gallery images"><i class="fa fa-circle-o"></i>Gallery</a></li>
+		<li><a href="#" title="Shows the complaints and suggessions"><i class="fa fa-circle-o"></i>Complaints & Suggessions</a></li>
+	</ul>
+</li>
+<?php endif; ?>
