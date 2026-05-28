6a78c73a96bd389aaa743b68e18f376f7b92361f
swarajpumex <swaraj@pumexinfotech.com>
2026-05-28 12:31:41 +0530

    Footer + fontsize & color in Quill

--- application/views/admin_panel/bottom-js.php
+++ application/views/admin_panel/bottom-js.php
@@ -37,6 +37,40 @@
 
 <script>
   window.quillInstances = window.quillInstances || {};
+  window.quillConfigReady = window.quillConfigReady || false;
+  var QUILL_SIZE_OPTIONS = ["12px", "14px", "16px", "18px", "20px", "24px", "30px", "36px", "48px"];
+
+  function ensureQuillSizePickerLabels(sizeOptions) {
+    var styleId = "quill-size-picker-labels";
+    if (document.getElementById(styleId)) {
+      return;
+    }
+
+    var css = [];
+    for (var i = 0; i < sizeOptions.length; i++) {
+      var val = sizeOptions[i];
+      css.push('.ql-snow .ql-picker.ql-size .ql-picker-item[data-value="' + val + '"]::before{content:"' + val + '";}');
+      css.push('.ql-snow .ql-picker.ql-size .ql-picker-label[data-value="' + val + '"]::before{content:"' + val + '";}');
+    }
+
+    var style = document.createElement("style");
+    style.id = styleId;
+    style.appendChild(document.createTextNode(css.join("\n")));
+    document.head.appendChild(style);
+  }
+
+  function configureQuillFormats() {
+    if (typeof Quill === "undefined" || window.quillConfigReady) {
+      return;
+    }
+
+    var Size = Quill.import("attributors/style/size");
+    Size.whitelist = QUILL_SIZE_OPTIONS;
+    Quill.register(Size, true);
+    ensureQuillSizePickerLabels(QUILL_SIZE_OPTIONS);
+
+    window.quillConfigReady = true;
+  }
 
   function getQuillHtmlValue(quill) {
     var html = quill && quill.root ? quill.root.innerHTML : "";
@@ -47,6 +81,8 @@
     if (typeof Quill === "undefined") {
       return null;
     }
+
+    configureQuillFormats();
 
     var textarea = document.getElementById(textareaId);
     if (!textarea) {
@@ -71,9 +107,14 @@
       modules: {
         toolbar: [
           [{
-            header: [1, 2, 3, false]
+            size: QUILL_SIZE_OPTIONS
           }],
           ["bold", "italic", "underline", "strike"],
+          [{
+            color: []
+          }, {
+            background: []
+          }],
           [{
             list: "ordered"
           }, {
--- application/views/public/Cinema/footer.php
+++ application/views/public/Cinema/footer.php
@@ -1,7 +1,35 @@
  <!-- Footer -->
+ <style>
+   .footer-inner {
+     display: flex;
+     flex-direction: row;
+     justify-content: space-between;
+     align-items: center;
+     padding: 10px 0;
+   }
+
+   .footer-inner span {
+     white-space: nowrap;
+   }
+
+   @media (max-width: 767px) {
+     .footer-inner {
+       flex-direction: column;
+       align-items: center;
+       gap: 6px;
+     }
+
+     .footer-inner span {
+       font-size: 13px;
+     }
+   }
+ </style>
  <footer class="bg-dark">
    <div class="container">
-     <p class="m-0 text-center text-white">Copyright &copy; Cinema News Agency 2025. <span style="float:right;color:#e3bc33;">Mail us to : cinemanewsagency@gmail.com</span></p>
+     <div class="footer-inner">
+       <span class="text-white">Copyright &copy; Cinema News Agency 2026.</span>
+       <span style="color:#e3bc33;">Mail us to : cinemanewsagency@gmail.com</span>
+     </div>
    </div>
    <!-- /.container -->
  </footer>
